5d13db86279c5b26929216ffa641c28c141f8aac
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-07-28 03:08:07 +0530

    fixed bug

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,42 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,'An account holder must have a name']
-    },
-    email:{
-        type:String,
-        required:[true,'An account holder must have an email'],
-        unique:true,
-        lowercase:true,
-        validate:[validator.isEmail,'The email address is not valid']
-    },
-    // photo:String,
-    password:{
-        type:String,
-        required:[true,'An account requires a password'],
-        minlength:8,
-        select:false
-    },
-    confirmPassword:{
-        type:String,
-        required:[true,'An account needs to confirm password'],
-        validate:[function(el){
-            return el===this.password;
-        },'Password and confirm Password do not match']
-    },
-    role:{
-        type:String,
-        enum:['user','admin','trainer'],
-        default:'user'
-    },
-    passwordChangedAt:Date,
-    passwordResetToken: String,
-    passwordResetTokenExpires : Date
-});
-
 const userModel = mongoose.Schema(
   {
     name: { type: String, required: true },
